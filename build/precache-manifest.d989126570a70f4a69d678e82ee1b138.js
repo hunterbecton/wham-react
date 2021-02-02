cf4e1909400f128be93b36be5c08d47dbc7510ec
hunterbecton <hunter@hunterbecton.com>
2021-02-02 14:59:24 -0500

    changed app and index.js folders

---
+++ build/precache-manifest.d989126570a70f4a69d678e82ee1b138.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf958dfa0da0210f8291cac6f877219",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b1b845f3f633381fc0",
+    "url": "/static/css/2.142bd2d8.chunk.css"
+  },
+  {
+    "revision": "4c4c9360f3058bb999fd",
+    "url": "/static/css/main.d517766c.chunk.css"
+  },
+  {
+    "revision": "55b1b845f3f633381fc0",
+    "url": "/static/js/2.8c67bd27.chunk.js"
+  },
+  {
+    "revision": "e1a0448b1fe71512389e8f49f758ce8a",
+    "url": "/static/js/2.8c67bd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4cb5b7af0313c7cc7e8",
+    "url": "/static/js/3.0e945252.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.0e945252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4c9360f3058bb999fd",
+    "url": "/static/js/main.dd4f8806.chunk.js"
+  },
+  {
+    "revision": "78da976932452187a049",
+    "url": "/static/js/runtime-main.473c8dfe.js"
+  },
+  {
+    "revision": "55c098e444ef5f2a97bf6ae967a7a073",
+    "url": "/static/media/arrowNext.55c098e4.svg"
+  },
+  {
+    "revision": "432555026f5b20d709d8f43fa1e76540",
+    "url": "/static/media/arrowPrev.43255502.svg"
+  },
+  {
+    "revision": "7d4c791281aab8bb7a5f1bd357c12a12",
+    "url": "/static/media/infinity.7d4c7912.svg"
+  },
+  {
+    "revision": "e55c459aaef5a79d2f798a40f573cce5",
+    "url": "/static/media/key.e55c459a.svg"
+  },
+  {
+    "revision": "a87f9c8fe6ac93cd58c931b1c1d212b0",
+    "url": "/static/media/loader.a87f9c8f.svg"
+  },
+  {
+    "revision": "87d544cdcc9e38d40e72a3a962186a38",
+    "url": "/static/media/logo.87d544cd.svg"
+  },
+  {
+    "revision": "28217797b8c98f302a674c50fbddd03a",
+    "url": "/static/media/sound.28217797.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
+import { Router } from '@reach/router'
+import { ThemeProvider } from 'styled-components'
+import 'react-toastify/dist/ReactToastify.css'
 
-const App = ({ children }) => {
-  // const location = useLocation()
+import { ProvideAuth } from './hooks/useAuth'
+import { ProvideSoundboard } from './hooks/useSoundboard'
+import { GlobalStyles } from './styles/GlobalStyles'
+import Theme from './styles/theme'
+import StyledToast from './components/Toast/StyledToast'
+import Homepage from './components/Homepage/Homepage'
+import SignupForm from './components/Auth/SignupForm'
+import LoginForm from './components/Auth/LoginForm'
+import Privacy from './components/Privacy/Privacy'
+import Tos from './components/Tos/Tos'
+import ForgotPassword from './components/Auth/ForgotPassword'
+import ResetPassword from './components/Auth/ResetPassword'
+import PrivateRoute from './components/Auth/PrivateRoute'
+import SoundboardEditor from './components/Editor/SoundboardEditor'
+import Soundboard from './components/Sound/Soundboard'
+import Dashboard from './components/Dashboard/Dashboard'
+import UpdateProfileForm from './components/Profile/UpdateProfileForm'
 
-  // const { isLoggedIn } = useAuth()
-
-  // useEffect(() => {
-  //   const runIsLoggedIn = async () => {
-  //     await isLoggedIn()
-  //   }
-
-  //   runIsLoggedIn()
-
-  // }, [location])
-
-  return <Fragment>{children}</Fragment>
+class App extends Component {
+  render() {
+    return (
+      <ProvideAuth>
+        <ProvideSoundboard>
+          <ThemeProvider theme={Theme}>
+            <GlobalStyles />
+            <StyledToast />
+            <Router>
+              <Homepage path="/" />
+              <SignupForm path="/signup" />
+              <LoginForm path="/login" />
+              <ForgotPassword path="/forgot-password" />
+              <ResetPassword path="/reset-password" />
+              <Privacy path="/privacy" />
+              <Tos path="/tos" />
+              <Soundboard path="/soundboard/:sbId" />
+              <PrivateRoute
+                as={SoundboardEditor}
+                roles={['user', 'pro', 'admin']}
+                path="/soundboard/create"
+              />
+              <PrivateRoute
+                as={SoundboardEditor}
+                roles={['user', 'pro', 'admin']}
+                path="/soundboard/edit/:sbId"
+              />
+              <PrivateRoute
+                as={Dashboard}
+                roles={['user', 'pro', 'admin']}
+                path="/dashboard"
+              />
+              <PrivateRoute
+                as={UpdateProfileForm}
+                roles={['user', 'pro', 'admin']}
+                path="/profile"
+              />
+            </Router>
+          </ThemeProvider>
+        </ProvideSoundboard>
+      </ProvideAuth>
+    )
+  }
 }
 
 export default App
