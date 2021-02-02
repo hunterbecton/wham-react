0d99f346cdb967f957e1ce0d7bab24938d1dfb26
hunterbecton <hunter@hunterbecton.com>
2021-02-02 14:24:51 -0500

    Changed router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Router } from '@reach/router'
 import { ThemeProvider } from 'styled-components'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 import { ProvideAuth } from './hooks/useAuth'
 import { ProvideSoundboard } from './hooks/useSoundboard'
 import { GlobalStyles } from './styles/GlobalStyles'
 import Theme from './styles/theme'
 import StyledToast from './components/Toast/StyledToast'
-import App from './App';
+import App from './App'
 import Homepage from './components/Homepage/Homepage'
 import SignupForm from './components/Auth/SignupForm'
 import LoginForm from './components/Auth/LoginForm'
@@ -31,42 +31,38 @@ ReactDOM.render(
           <GlobalStyles />
           <StyledToast />
           <Router>
-            <App path="/">
-              <SignupForm path="/signup" />
-              <LoginForm path="/login" />
-              <ForgotPassword path='/forgot-password' />
-              <ResetPassword path='/reset-password' />
-              <Privacy path='/privacy' />
-              <Tos path='/tos' />
-              <Homepage path="/" />
-              <Soundboard
-                path="/soundboard/:sbId"
-              />
-              <PrivateRoute
-                as={SoundboardEditor}
-                roles={['user', 'pro', 'admin']}
-                path="/soundboard/create"
-              />
-              <PrivateRoute
-                as={SoundboardEditor}
-                roles={['user', 'pro', 'admin']}
-                path="/soundboard/edit/:sbId"
-              />
-              <PrivateRoute
-                as={Dashboard}
-                roles={['user', 'pro', 'admin']}
-                path="/dashboard"
-              />
-              <PrivateRoute
-                as={UpdateProfileForm}
-                roles={['user', 'pro', 'admin']}
-                path="/profile"
-              />
-            </App>
+            <Homepage path="/" />
+            <SignupForm path="/signup" />
+            <LoginForm path="/login" />
+            <ForgotPassword path="/forgot-password" />
+            <ResetPassword path="/reset-password" />
+            <Privacy path="/privacy" />
+            <Tos path="/tos" />
+            <Soundboard path="/soundboard/:sbId" />
+            <PrivateRoute
+              as={SoundboardEditor}
+              roles={['user', 'pro', 'admin']}
+              path="/soundboard/create"
+            />
+            <PrivateRoute
+              as={SoundboardEditor}
+              roles={['user', 'pro', 'admin']}
+              path="/soundboard/edit/:sbId"
+            />
+            <PrivateRoute
+              as={Dashboard}
+              roles={['user', 'pro', 'admin']}
+              path="/dashboard"
+            />
+            <PrivateRoute
+              as={UpdateProfileForm}
+              roles={['user', 'pro', 'admin']}
+              path="/profile"
+            />
           </Router>
         </ThemeProvider>
       </ProvideSoundboard>
     </ProvideAuth>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
